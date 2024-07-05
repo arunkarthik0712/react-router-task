333e2c14398a7cc0a7e283c769543429231123fe
arunkarthik0712 <arunkarthik0710@gmail.com>
2024-07-06 00:39:06 +0530

    blog data

---
+++ src/data/blogs.js
@@ -0,0 +1,263 @@
+// public/data/blogs.js
+const blogs = {
+  all: [
+    {
+      id: 1,
+      title: "Best Full-Stack Development Project Ideas in 2024",
+      image: "/images/person-front-computer-working-html.jpg",
+    },
+    {
+      id: 2,
+      title: "How Long Would It Take to Be a Full Stack Developer?",
+      image:
+        "/images/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp",
+    },
+    {
+      id: 3,
+      title: "10 Best Database Management Systems For Software Developers",
+      image: "/images/DBMS1.webp",
+    },
+    {
+      id: 4,
+      title: "How does Apache work? A detailed introduction to Apache",
+      image: "/images/1_6rDcIgFJQldloIERiUSmzw.webp",
+    },
+    {
+      id: 5,
+      title:
+        "Hot Topics That You Need To Know In Full Stack Developer Syllabus | 2024",
+      image: "/images/Full-Stack-Developer-course-syllabus.webp",
+    },
+    {
+      id: 6,
+      title: "Top 10 Full-Stack Developer Frameworks in 2024",
+      image: "/images/html-css-collage-concept-with-person.jpg",
+    },
+    {
+      id: 7,
+      title: "7 Unique Web Development Project Ideas for Beginners",
+      image: "/images/Feature-3.webp",
+    },
+    {
+      id: 8,
+      title: "6 Essential Prerequisites For Learning ReactJS",
+      image: "/images/react-600x314.jpg",
+    },
+    {
+      id: 9,
+      title: "Best Web Development Roadmap for Beginners 2024",
+      image: "/images/1.jpg",
+    },
+    {
+      id: 10,
+      title: "Top 10 High Paying Non-Coding Jobs in Data Science in 2024",
+      image:
+        "/images/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science-768x480.webp",
+    },
+    {
+      id: 11,
+      title: "12 Real-World Data Science Examples: Power Of Data Science",
+      image: "/images/Real-World-Data-Science-Examples-1536x804.webp",
+    },
+    {
+      id: 12,
+      title: "Can A Commerce Student Do Data Science?",
+      image: "/images/5.webp",
+    },
+    {
+      id: 13,
+      title: "Roles and Responsibilities of a Data Scientist",
+      image: "/images/image-4-1536x866.webp",
+    },
+    {
+      id: 14,
+      title: "Top Product-Based Companies for Data Scientists in 2024",
+      image:
+        "/images/Feature-image-Top-Product-Based-Companies-for-Data-Scientists-768x480.webp",
+    },
+    {
+      id: 15,
+      title: "10 Best Data Science Online blogs for Beginners | 2024",
+      image:
+        "/images/feature-image-Best-Data-Science-Online-Courses-for-Beginners.webp",
+    },
+    {
+      id: 16,
+      title: "Cyber Security Essentials",
+      image: "/images/dark-web.png",
+    },
+    {
+      id: 17,
+      title: "Cybersecurity Vs Ethical Hacking: Top 10 Differences",
+      image: "/images/8bfd01c18be1b5059bc0d7770d9dabf1.webp",
+    },
+    {
+      id: 18,
+      title: "Non-Coding Jobs in Cybersecurity: A Comprehensive Guide",
+      image:
+        "/images/Feature-image-Non-Coding-Jobs-in-Cybersecurity-768x480.webp",
+    },
+    {
+      id: 19,
+      title: "What Is Hacking? Types of Hacking & More",
+      image:
+        "/images/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-768x480.webp",
+    },
+    {
+      id: 20,
+      title: "8 Different Types of Cybersecurity and Threats Involved",
+      image:
+        "/images/Teal-Illustration-Digital-Business-Blog-Banner-1-1536x864.webp",
+    },
+    {
+      id: 21,
+      title: "Career Development Strategies",
+      image: "/images/feature-image-unique-project-ideas.jpg",
+    },
+    {
+      id: 22,
+      title:
+        "40 Java Interview Questions for Freshers with Clear & Concise Answers",
+      image: "/images/java.jpeg",
+    },
+    {
+      id: 23,
+      title: "Top 30 Mini Project Ideas For College Students",
+      image: "/images/blog-2-600x314.webp",
+    },
+  ],
+  fullstack: [
+    {
+      id: 1,
+      title: "Best Full-Stack Development Project Ideas in 2024",
+      image: "/images/person-front-computer-working-html.jpg",
+    },
+    {
+      id: 2,
+      title: "How Long Would It Take to Be a Full Stack Developer?",
+      image:
+        "/images/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp",
+    },
+    {
+      id: 3,
+      title: "10 Best Database Management Systems For Software Developers",
+      image: "/images/DBMS1.webp",
+    },
+    {
+      id: 4,
+      title: "How does Apache work? A detailed introduction to Apache",
+      image: "/images/1_6rDcIgFJQldloIERiUSmzw.webp",
+    },
+    {
+      id: 5,
+      title:
+        "Hot Topics That You Need To Know In Full Stack Developer Syllabus | 2024",
+      image: "/images/Full-Stack-Developer-course-syllabus.webp",
+    },
+    {
+      id: 6,
+      title: "Top 10 Full-Stack Developer Frameworks in 2024",
+      image: "/images/html-css-collage-concept-with-person.jpg",
+    },
+    {
+      id: 7,
+      title: "7 Unique Web Development Project Ideas for Beginners",
+      image: "/images/Feature-3.webp",
+    },
+    {
+      id: 8,
+      title: "6 Essential Prerequisites For Learning ReactJS",
+      image: "/images/react-600x314.jpg",
+    },
+    {
+      id: 9,
+      title: "Best Web Development Roadmap for Beginners 2024",
+      image: "/images/1.jpg",
+    },
+  ],
+  datascience: [
+    {
+      id: 10,
+      title: "Top 10 High Paying Non-Coding Jobs in Data Science in 2024",
+      image:
+        "/images/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science-768x480.webp",
+    },
+    {
+      id: 11,
+      title: "12 Real-World Data Science Examples: Power Of Data Science",
+      image: "/images/Real-World-Data-Science-Examples-1536x804.webp",
+    },
+    {
+      id: 12,
+      title: "Can A Commerce Student Do Data Science?",
+      image: "/images/5.webp",
+    },
+    {
+      id: 13,
+      title: "Roles and Responsibilities of a Data Scientist",
+      image: "/images/image-4-1536x866.webp",
+    },
+    {
+      id: 14,
+      title: "Top Product-Based Companies for Data Scientists in 2024",
+      image:
+        "/images/Feature-image-Top-Product-Based-Companies-for-Data-Scientists-768x480.webp",
+    },
+    {
+      id: 15,
+      title: "10 Best Data Science Online blogs for Beginners | 2024",
+      image:
+        "/images/feature-image-Best-Data-Science-Online-Courses-for-Beginners.webp",
+    },
+  ],
+  cybersecurity: [
+    {
+      id: 16,
+      title: "Cyber Security Essentials",
+      image: "/images/dark-web.png",
+    },
+    {
+      id: 17,
+      title: "Cybersecurity Vs Ethical Hacking: Top 10 Differences",
+      image: "/images/8bfd01c18be1b5059bc0d7770d9dabf1.webp",
+    },
+    {
+      id: 18,
+      title: "Non-Coding Jobs in Cybersecurity: A Comprehensive Guide",
+      image:
+        "/images/Feature-image-Non-Coding-Jobs-in-Cybersecurity-768x480.webp",
+    },
+    {
+      id: 19,
+      title: "What Is Hacking? Types of Hacking & More",
+      image:
+        "/images/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-768x480.webp",
+    },
+    {
+      id: 20,
+      title: "8 Different Types of Cybersecurity and Threats Involved",
+      image:
+        "/images/Teal-Illustration-Digital-Business-Blog-Banner-1-1536x864.webp",
+    },
+  ],
+  career: [
+    {
+      id: 21,
+      title: "Career Development Strategies",
+      image: "/images/feature-image-unique-project-ideas.jpg",
+    },
+    {
+      id: 22,
+      title:
+        "40 Java Interview Questions for Freshers with Clear & Concise Answers",
+      image: "/images/java.jpeg",
+    },
+    {
+      id: 23,
+      title: "Top 30 Mini Project Ideas For College Students",
+      image: "/images/blog-2-600x314.webp",
+    },
+  ],
+};
+
+export default blogs;
