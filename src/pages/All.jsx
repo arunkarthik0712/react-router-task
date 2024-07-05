007da2bada604e5d744729d8548c97d5d6381690
arunkarthik0712 <arunkarthik0710@gmail.com>
2024-07-06 00:39:32 +0530

    all page component

---
+++ src/pages/All.jsx
@@ -0,0 +1,22 @@
+// src/pages/AllBlogs.jsx
+import blogs from "../data/blogs";
+
+function AllBlogs() {
+  return (
+    <div className="row">
+      {blogs.all.map((blog) => (
+        <div className="col-md-4 mb-4" key={blog.id}>
+          <div className="card">
+            <img src={blog.image} className="card-img-top" alt={blog.title} />
+            <div className="card-body">
+              <h5 className="card-title">{blog.title}</h5>
+              <p className="card-text text-end">read more></p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AllBlogs;
